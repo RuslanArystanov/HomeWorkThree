f6b4291b74ab0ebea182812a4d8fa3b623609cd3
ruslanarystanov <r.arystanov@arbuz.kz>
2024-10-07 10:51:40 +0500

    Home Work Three - Autorization

--- HomeWorkThree/UserPajeViewController.swift
+++ HomeWorkThree/UserPajeViewController.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class UserPajeViewController: UIViewController {
-
+    @IBOutlet var viewBackgroundColor: UIView!
+    @IBOutlet var welcomeMessage: UILabel!
+    var userName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1).cgColor, #colorLiteral(red: 0.4449194074, green: 0.788621366, blue: 0.918802619, alpha: 1).cgColor]
+        gradientLayer.shouldRasterize = true
+        viewBackgroundColor.layer.insertSublayer(gradientLayer, at: 0)
+        
+        welcomeMessage.text = "Welcome, \(String(userName))"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- HomeWorkThree/ViewController.swift
+++ HomeWorkThree/ViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var userNameTexField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var forgotUserNameButton: UIButton!
+    
+    private let userLogin = "Ruslan"
+    private let userPassword = "Rusal"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userNameTexField.returnKeyType = .next
+        passwordTextField.returnKeyType = .done
+        passwordTextField.isSecureTextEntry = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let userPage = segue.destination as? UserPajeViewController else {
+            return
+        }
+        
+        userPage.userName = userNameTexField.text
     }
 
-
+    @IBAction func logIn() {
+        if passwordTextField.text == userPassword && userNameTexField.text == userLogin{
+            self.performSegue(withIdentifier: "welcomePage", sender: self)
+        } else {
+            showAlert(
+                "Invalid login or password",
+                "Please, enter correct login and password"
+            )
+        }
+    }
+    
+    @IBAction func promptUserInformation(_ sender: UIButton) {
+        var promptMessage = ""
+        
+        switch sender {
+        case forgotUserNameButton:
+            promptMessage = "Your name is \(userLogin) 😉"
+        default:
+            promptMessage = "Your password is \(userPassword) 😉"
+        }
+        
+        showAlert("Oops!", promptMessage)
+    }
+    
+    private func showAlert(_ title: String, _ message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTexField.text = nil
+        passwordTextField.text = nil
+    }
+    
 }
+
 
